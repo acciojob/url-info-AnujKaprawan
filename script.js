912eaa286e1ade736217c0b719d6fdf0103d6eed
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-03-02 20:48:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// let currentUrl = window.location.href;
+//  let length  = currentUrl.length;
+// alert(`The length of the URL is ${length}`)
+// Get the current URL
+	let currentUrl = window.location.href;
+     let length = currentUrl.length;
+alert(`The length of the URL is: ${length}`);
+// alert("the length of the url :" +length);
